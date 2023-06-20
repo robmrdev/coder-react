0d93b4598d71855b879cd17e68adf24ff7aa1747
Robert Mendoza <robmrivas@gmail.com>
2023-06-20 19:05:58 -0300

    Reupload NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './app.css';
+import './App.css';
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
