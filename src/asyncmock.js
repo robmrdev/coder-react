b133bd6c7a9d3e5e4754deab4d793f61f53d37ff
Robert Mendoza <robmrivas@gmail.com>
2023-06-28 20:16:56 -0300

    Item img change source

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,35 +1,35 @@
 const misProductos = [
     {
         id: "1",
-        img: "src/img/items/chairs/karlpetter-green.avif",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/karlpetter-green.avif",
         name: "KARLPETTER",
         price: "55",
         description: "Chair",
-        hoverImg: "src/img/items/chairs/karlpetter-green-bg1.avif",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/karlpetter-green-bg1.avif",
     },
     {
         id: "2",
-        img: "src/img/items/chairs/ostano-black.avif",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/ostano-black.avif",
         name: "ÖSTANÖ",
         price: "20",
         description: "Chair",
-        hoverImg: "src/img/items/chairs/ostano-black-bg1.jpg",
+        hoverImg: "https://github.com/robmrivas/coder-react/blob/main/src/img/items/chairs/ostano-black-bg1.jpg",
     },
     {
         id: "3",
-        img: "src/img/items/chairs/lidas-white.avif",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/lidas-white.avif",
         name: "LIDÅS",
         price: "45",
         description: "Chair",
-        hoverImg: "src/img/items/chairs/lidas-white-bg1.avif",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/lidas-white-bg1.avif",
     },
     {
         id: "4",
-        img: "src/img/items/chairs/poang-beige.avif",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/poang-beige.avif",
         name: "POÄNG",
         price: "70",
         description: "Armchair",
-        hoverImg: "src/img/items/chairs/poang-beige-bg1.avif",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/poang-beige-bg1.avif",
     },
 ];
 
