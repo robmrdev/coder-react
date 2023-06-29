28af481a140efbcd8b7635c1c87403c2e7d6ead8
Robert Mendoza <robmrivas@gmail.com>
2023-06-29 15:04:32 -0300

    adding wardroves

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,6 +1,6 @@
 const misProductos = [
     {
-        id: "1",
+        id: "001",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/karlpetter-green.avif",
         name: "KARLPETTER",
         price: "55",
@@ -8,7 +8,7 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/karlpetter-green-bg1.avif",
     },
     {
-        id: "2",
+        id: "002",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/ostano-black.avif",
         name: "ÖSTANÖ",
         price: "20",
@@ -16,7 +16,7 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/ostano-black-bg1.avif",
     },
     {
-        id: "3",
+        id: "003",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/lidas-white.avif",
         name: "LIDÅS",
         price: "45",
@@ -24,7 +24,7 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/lidas-white-bg1.avif",
     },
     {
-        id: "4",
+        id: "004",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/poang-beige.avif",
         name: "POÄNG",
         price: "70",
@@ -32,7 +32,7 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/chairs/poang-beige-bg1.avif",
     },
     {
-        id: "5",
+        id: "005",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/micke-white.avif",
         name: "MICKE",
         price: "90",
@@ -40,7 +40,7 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/micke-white-bg1.avif",
     },
     {
-        id: "6",
+        id: "006",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/kallax-white.avif",
         name: "KALLAX",
         price: "150",
@@ -48,7 +48,7 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/kallax-white-bg1.webp",
     },
     {
-        id: "7",
+        id: "007",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/malm-white.webp",
         name: "MALM",
         price: "169",
@@ -56,12 +56,44 @@ const misProductos = [
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/malm-white-bg1.webp",
     },
     {
-        id: "8",
+        id: "008",
         img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/lagkapten-white.avif",
         name: "LAGKAPTEN",
         price: "109",
         description: "Desk, 120x60 cm",
         hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/desks/lagkapten-white-bg1.avif",
+    },
+    {
+        id: "009",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/kleppstad-white.avif",
+        name: "KLEPPSTAD",
+        price: "149",
+        description: "Wardrobe with 3 doors, white, 117x176 cm",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/kleppstad-white-bg1.avif",
+    },
+    {
+        id: "010",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/rakkestad-black.avif",
+        name: "RAKKESTAD",
+        price: "179",
+        description: "Wardrobe with 3 doors, black-brown, 117x176 cm",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/rakkestad-black-bg1.avif",
+    },
+    {
+        id: "011",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/brimnes-white.webp",
+        name: "BRIMNES",
+        price: "229",
+        description: "Wardrobe with 3 doors, white, 117x190 cm",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/brimnes-white-bg1.webp",
+    },
+    {
+        id: "012",
+        img: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/gursken-beige.webp",
+        name: "GURSKEN",
+        price: "59",
+        description: "Wardrobe, light beige, 49x55x186 cm",
+        hoverImg: "https://raw.githubusercontent.com/robmrivas/coder-react/main/src/img/items/wardroves/gursken-beige-bg1.webp",
     },
 ];
 
